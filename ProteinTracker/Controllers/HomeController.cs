7ff8efe95b77d426ed2def81bf16ca21ec4a96d7
Gregory Kan <kan.gregory@gmail.com>
2015-08-05 17:43:07 +1200

    Wiring out views

---
+++ ProteinTracker/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ServiceStack.Redis;
+using ProteinTracker.Models;
+
+namespace ProteinTracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            using (IRedisClient client = new RedisClient())
+            {
+                var userClient = client.As<User>();
+                var users = userClient.GetAll(); //get stuff in the database
+                var usersSelection = new SelectList(users, "Id", "Name", String.Empty);
+                ViewBag.Users = usersSelection;
+
+            }
+
+            return View();
+
+
+
+        }
+    }
+}
---
+++ ProteinTracker/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ProteinTracker.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult NewUser()
+        {
+            return View();
+        }
+    }
+}
